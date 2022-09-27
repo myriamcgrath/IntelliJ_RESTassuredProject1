96b7a0f144efe9fed9245384829f33cb714fa663
Myria McGrath <mmcgrath@mi9retail.com>
2022-09-27 19:54:40 -0400

    Initial commit one

---
+++ src/test/java/APITests.java
@@ -0,0 +1,29 @@
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+
+public class APITests {
+    @Test
+    void test1(){
+        Response response = get("https://reqres.in/api/users?page=2");
+
+        System.out.println("Response : "+response.asString());
+        System.out.println("Status Code : "+response.getStatusCode());
+        System.out.println("Body : " + response.getBody().asString());
+        System.out.println("Time Taken: " +response.getTime());
+        System.out.println("Header : "+response.getHeader("content-type"));
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+    @Test
+    void test2(){
+        given().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                statusCode(200);
+    }
+}
